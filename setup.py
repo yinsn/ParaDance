6d7b53c5aa7ad46b4079f92a8c8bb460f8a09343
yinsn <ycheng_sjtu@foxmail.com>
2023-05-23 15:13:08 +0800

    add configs

---
+++ setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+SETUP_DIRECTORY = Path(__file__).resolve().parent
+
+with (SETUP_DIRECTORY / "README.md").open() as ifs:
+    LONG_DESCRIPTION = ifs.read()
+
+install_requires = (
+    [
+        "pandas",
+    ],
+)
+
+setup(
+    name="hiveviewer",
+    version="0.0.1",
+    author="Yin Cheng",
+    author_email="yin.sjtu@gmail.com",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yinsn/hiveviewer",
+    python_requires=">=3.7",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=install_requires,
+    include_package_data=True,
+)
