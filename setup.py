16b0f30c2961f000a1ffe7d37340f8edebf6e5aa
yinsn <ycheng_sjtu@foxmail.com>
2023-06-12 17:13:11 +0800

    update version for v0.0.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = (
 
 setup(
     name="hiveviewer",
-    version="0.0.1",
+    version="0.0.2",
     author="Yin Cheng",
     author_email="yin.sjtu@gmail.com",
     long_description=LONG_DESCRIPTION,
