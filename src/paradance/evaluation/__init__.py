a45a34db4eb48940fff6db4da1099e95f8bc5913
chengyin <ycheng_sjtu@foxmail.com>
2024-03-15 15:40:18 +0800

    Introduce 'SelfBalancingLogarithmPCACalculator' as an alternative option for 'Calculator'

--- src/paradance/evaluation/__init__.py
+++ src/paradance/evaluation/__init__.py
@@ -1,3 +1,7 @@
 from .calculator import Calculator
+from .logarithm_pca_calculator import PCACalculator
 
-__all__ = ["Calculator"]
+__all__ = [
+    "Calculator",
+    "PCACalculator",
+]
---
+++ src/paradance/evaluation/logarithm_pca_calculator.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from mixician import SelfBalancingLogarithmPCACalculator
+
+
+class PCACalculator:
+    """A calculator for performing PCA (Principal Component Analysis) operations.
+
+    This calculator uses an instance of a SelfBalancingLogarithmPCACalculator to perform
+    the underlying PCA calculations and updates.
+
+    Attributes:
+        pca_calculator (SelfBalancingLogarithmPCACalculator): The PCA calculator instance
+            used for performing PCA operations.
+        df (DataFrame): A copy of the cleaned dataframe from the `pca_calculator`.
+    """
+
+    def __init__(self, pca_calculator: SelfBalancingLogarithmPCACalculator):
+        self.pca_calculator = pca_calculator
+        self.df = self.pca_calculator.clean_dataframe.copy()
+
+    def get_overall_score(
+        self,
+        weights_for_equation: List[float],
+    ) -> None:
+        """
+        Calculates and assigns an overall score to each entry in the dataframe based on
+        the provided weights for the PCA equation.
+
+        This method updates the internal dataframe `df` with a new column `overall_score`
+        that contains the calculated scores for each entry.
+
+        Args:
+            weights_for_equation (List[float]): A list of weights for calculating the
+                overall PCA score.
+        """
+        self.pca_calculator.update(
+            pca_weights=weights_for_equation,
+        )
+        self.df["overall_score"] = self.pca_calculator.cumulative_product_scores
