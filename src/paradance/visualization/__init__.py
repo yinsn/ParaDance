cb158dc37fdb5e68750f3e20c504453ce9f15545
yin <ycheng_sjtu@foxmail.com>
2024-04-04 22:16:35 +0800

    Remove dependency on Venn diagrams

--- src/paradance/visualization/__init__.py
+++ src/paradance/visualization/__init__.py
@@ -1,15 +1,5 @@
-from .lorenz_curve import LorenzCurveGini
-from .plot_trisurf import TrisurfPloter
 from .portfolio_curve import PortfolioPlotter
-from .venn2 import Venn2Ploter
-from .venn3 import Venn3Ploter
-from .venn_base import BaseVennPloter
 
 __all__ = [
-    "LorenzCurveGini",
-    "BaseVennPloter",
-    "Venn3Ploter",
-    "Venn2Ploter",
-    "TrisurfPloter",
     "PortfolioPlotter",
 ]
