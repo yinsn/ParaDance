e4ba0699797ac3d48e3617620271e6c1c5ca813b
chengyin <ycheng_sjtu@foxmail.com>
2024-05-11 14:37:08 +0800

    Expose 'map_to_bins' function for external use

--- src/paradance/evaluation/__init__.py
+++ src/paradance/evaluation/__init__.py
@@ -2,10 +2,12 @@ from .base_calculator import BaseCalculator
 from .base_evaluator import evaluation_preprocessor
 from .calculator import Calculator
 from .logarithm_pca_calculator import LogarithmPCACalculator
+from .tau_evaluator import map_to_bins
 
 __all__ = [
     "BaseCalculator",
     "Calculator",
     "evaluation_preprocessor",
     "LogarithmPCACalculator",
+    "map_to_bins",
 ]
